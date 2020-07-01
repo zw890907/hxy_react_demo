6ca371ba15534558900884074f4a3053ae168f2a
zhangwei <514970358@qq.com>
2020-07-01 15:04:54 +0800

    添加矢量图标

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,6 +12,7 @@ class Home extends Component {
             <div className="home">
                 <p>首页内容</p>
                 <Button type="primary">点击</Button>
+                <i class="iconfont icon-shanchu2"></i>
             </div>
         )
     }
